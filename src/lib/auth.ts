8c1e5935ebc8124019f5721e52a5143e0ef673a8
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2025-02-23 20:37:45 +0500

    basic auth setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,27 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { AuthOptions } from "next-auth"
+import { prisma } from "./db"
+import Google from "next-auth/providers/google"
+import Email from "next-auth/providers/email"
+
+export const authOptions:AuthOptions = {
+    adapter:PrismaAdapter(prisma),
+    providers:[
+        Google({
+            clientId:process.env.GOOGLE_CLIENT_ID!,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+        Email({
+            server: {
+              host: process.env.EMAIL_SERVER_HOST!,
+              port: parseInt(process.env.EMAIL_SERVER_PORT!),
+              auth: {
+                user: process.env.EMAIL_SERVER_USER!,
+                pass: process.env.EMAIL_SERVER_PASSWORD!,
+              },
+            },
+            from: process.env.EMAIL_FROM!,
+          }),
+
+    ]
+}
